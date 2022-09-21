8217e102a57fe9223fd3caa8dc759bda0d2e1dbf
Faye Rosenshein <rosefaye.a@gmail.com>
2022-09-21 09:14:16 -0600

    Call 3 different elements from my arrays

--- arrays.js
+++ arrays.js
@@ -3,3 +3,12 @@ var friends = ["Adriana","Molly","Katy","Clayton"];
 var ages = [ 26, 23, 27, 25];
 
 var hasPets = [false, true, true, false];
+
+  // I am calling Adriana, the first
+console.log(friends[0]);
+
+// I am calling Molly's age
+console.log(ages[1]);
+
+// I am calling Katy's status of having a pet
+console.log(hasPets[2]);
