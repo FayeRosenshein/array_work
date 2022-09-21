753662764bf13a0fca7f6fa33f884935b7cd56eb
Faye Rosenshein <rosefaye.a@gmail.com>
2022-09-21 09:22:39 -0600

    Show the comments I have added

--- arrays.js
+++ arrays.js
@@ -12,3 +12,9 @@ console.log(ages[1]);
 
 // I am calling Katy's status of having a pet
 console.log(hasPets[2]);
+
+// I know that the initial position in an array starts with 0 so every number from the array after will be n-1
+// n = number of objects in the array
+// element = n-1
+// Such as the element 4 will be the fifth object in the array
+// And the for the fifth object in the array it will be the element number 4
